656e558b869517ef5ece185d6c7c2c279202e91b
Pranav Chandra <pranavchandra27@gmail.com>
2020-03-15 22:51:39 +0530

    start responsiveness

--- src/components/Photos/Photo.js
+++ src/components/Photos/Photo.js
@@ -14,8 +14,46 @@ export class Photo extends Component {
           }}
           className="Photo-Layer"
         ></div>
+        <div className="small-display">
+          <div className="pb-3">
+            <div className="d-flex align-items-center">
+              <Link to={`/user/${photo.user.username}`}>
+                <img
+                  className="rounded-circle"
+                  src={photo.user.profile_image.large}
+                  alt={photo.user.name}
+                />
+              </Link>
+              <Link to={`/user/${photo.user.username}`}>
+                <p className="pl-3 m-0">{photo.user.name}</p>
+              </Link>
+            </div>
+          </div>
+        </div>
         <div style={{ background: photo.color }}>
           <img src={photo.urls.small} alt={photo.alt_description} />
+        </div>
+        <div className="small-display ">
+          <div className="pt-2 d-flex justify-content-between">
+            <div className="d-flex">
+              <p className="m-0">
+                <i className="far fa-heart"></i>
+              </p>
+              <p className="m-0 pl-3">
+                <i className="far fa-plus-square"></i>
+              </p>
+            </div>
+            <div>
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href={photo.links.download}
+                className="text-dark"
+              >
+                <i className="fas fa-download"></i>
+              </a>
+            </div>
+          </div>
         </div>
         <div className="Photo-Info">
           <div className="Top-Icon">
